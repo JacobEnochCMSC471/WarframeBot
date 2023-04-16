4b9fd99a8edf0aafe37d9276ec1144160fc5d5bc
Jacob Enoch <79029984+JacobEnochCMSC471@users.noreply.github.com>
2023-04-16 14:08:52 -0400

    Added ability for user to request relics needed for items. Needs refinement.

--- JsonToMongo.py
+++ JsonToMongo.py
@@ -4,30 +4,14 @@
 # # JSON Digestion File
 # This is used to transfer many .json files into MongoDB. 
 
-# In[1]:
-
-
-import sys
-get_ipython().system('{sys.executable} -m pip install pymongo')
-get_ipython().system('pip freeze')
+# get_ipython().system('{sys.executable} -m pip install pymongo')
+# get_ipython().system('pip freeze')
 from pymongo import MongoClient
-
-
-# In[68]:
-
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client['WarframeRelics']
 
-
-# In[69]:
-
-
 collection_list = ["Axi", "Neo", "Meso", "Lith"]
-
-
-# In[70]:
-
 
 axi_dir = "WFCD warframe-drop-data gh-pages data-relics/Axi"
 neo_dir = "WFCD warframe-drop-data gh-pages data-relics/Neo"
@@ -36,11 +20,6 @@ lith_dir = "WFCD warframe-drop-data gh-pages data-relics/Lith"
 
 relic_tiers = [axi_dir, neo_dir, meso_dir, lith_dir]
 
-
-# In[71]:
-
-
-import os
 import json
 from pathlib import Path
 
@@ -53,31 +32,16 @@ for tier in relic_tiers:
 
     for file in files:
         temp_list.append(file)
-    
+
     filepath_lists.append(temp_list)
-
-
-# In[75]:
-
 
 for index in range(4):
     curr_collection = db.get_collection(collection_list[index])
     curr_tier = filepath_lists[index]
     print(curr_collection)
-    
+
     for relic in curr_tier:
         with open(relic) as file:
             file_json = json.load(file)
             curr_collection.insert_one(file_json)
         file.close()
-
-
-# In[52]:
-
-
-for item in all_paths:
-    with open(item) as file:
-        file_json = json.load(file)
-        relic_collection.insert_one(file_json)
-    file.close()
-
--- bot.py
+++ bot.py
@@ -1,29 +1,95 @@
 import discord
-from discord.ext import commands
+from discord import app_commands
 import os
 from dotenv import load_dotenv
+import logging
+from pymongo import MongoClient
+import utility
 
 load_dotenv()
 token = os.getenv("API_KEY")
+guild_id = os.getenv("GUILD_KEY")
+intents = discord.Intents.all()
 
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+try:
+    db_client = MongoClient("mongodb://localhost:27017/")
+    relic_db = db_client['WarframeRelics']
+
+    axi_collection = relic_db.get_collection("Axi")
+    neo_collection = relic_db.get_collection("Neo")
+    meso_collection = relic_db.get_collection("Meso")
+    lith_collection = relic_db.get_collection("Lith")
+
+except:
+    print(
+        "Database likely not connected or configured correctly. Please modify connection settings or make sure you have MongoDB and a server running with the required data.")
+    exit(0)
 
 
-async def on_ready(self):
-    print(f'Logged on as {self.user}!')
+class Client(discord.Client):
+    def __init__(self):
+        super().__init__(intents=intents)
+        self.synced = False  # added to make sure that the command tree will be synced only once
+        self.added = False
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:  # check if slash commands have been synced
+            await tree.sync(guild=discord.Object(guild_id))
+            self.synced = True
+        if not self.added:
+            self.added = True
+
+
+client = Client()
+tree = app_commands.CommandTree(client)
+
+
+@tree.command(name="relic_request", guild=discord.Object(guild_id), description="Look up what relics are needed for primed items!")
+async def relic_request(interaction: discord.Interaction, item: str):
+    # Make sure that the entered text only consists of characters
+    if all(x.isalpha() or x.isspace() for x in item):
+        userQuery = {'rewards.Intact.itemName': item}
+        axiQueryResult = axi_collection.find(userQuery)
+        neoQueryResult = neo_collection.find(userQuery)
+        mesoQueryResult = meso_collection.find(userQuery)
+        lithQueryResult = lith_collection.find(userQuery)
+
+        combinedQueries = [axiQueryResult, neoQueryResult, mesoQueryResult, lithQueryResult]
+        relicList = []
+
+        for queryResult in combinedQueries:
+            for indvResult in queryResult:
+                itemIndex = utility.search_list(indvResult['rewards']['Intact'], item)
+                tempItemName = indvResult['rewards']['Intact'][itemIndex]['itemName']
+                tempTier = indvResult['tier']
+                tempName = indvResult['name']
+
+                relicList.append({'tier': tempTier, 'name': tempName, 'itemName': tempItemName})
+
+        botRelicResponse = '-----Relics Needed For {}-----\n'.format(relicList[0]['itemName'])
+
+        for item in relicList:
+            relicText = "{} {}\n".format(item['tier'], item['name'])
+            botRelicResponse += relicText
+
+        await interaction.response.send_message(botRelicResponse)
+
+    else:
+        await interaction.response.send_message("{}, please enter a sequence of letters only.".format(interaction.user.mention))
+        return
 
 
 @client.event
 async def on_message(message):
-    if message.author.bot:  # Do not respond to other bots
+    if message.author == client.user:
         return
 
-    if message.author.id in [client.user.id]:  # Prevent infinite spamming by responding to itself
-        return
 
-    print(f'Message from {message.author}: {message.content}')
-    return
+@client.event
+async def on_command_error(ctx, error):
+    pass
+
 
 def main():
     client.run(token)
