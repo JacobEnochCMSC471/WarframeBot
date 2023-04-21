65a660040b9717f4cc9d64a251270ae805fce727
Jacob Enoch <79029984+JacobEnochCMSC471@users.noreply.github.com>
2023-04-21 13:49:03 -0400

    Almost finished refactoring; partial querying now works; working on error handling/tests

--- bot.py
+++ bot.py
@@ -16,15 +16,16 @@ intents = discord.Intents.all()
 try:
     db_client = MongoClient("mongodb://localhost:27017/")
     relic_db = db_client['WarframeRelics']
-
-    axi_collection = relic_db.get_collection("Balls")
-    neo_collection = relic_db.get_collection("Neo")
-    meso_collection = relic_db.get_collection("Meso")
-    lith_collection = relic_db.get_collection("Lith")
+    relic_collections = utility.populate_collections_list(relic_db)
 
 except errors.ConnectionFailure:
     print(
         "Database likely not connected or configured correctly. Please modify connection settings or make sure you have MongoDB and a server running with the required data.")
+    exit(0)
+
+except errors.PyMongoError as generic_error:
+    print('Some other PyMongo error occurred, see printed text for details.')
+    print(generic_error)
     exit(0)
 
 
@@ -50,40 +51,17 @@ tree = app_commands.CommandTree(client)
 @tree.command(name="relic_request", guild=discord.Object(guild_id), description="Look up what relics are needed for primed items!")
 async def relic_request(interaction: discord.Interaction, item: str):
     # Make sure that the entered text only consists of characters or spaces
+
     if all(char.isalpha() or char.isspace() for char in item):
+        query_results = utility.get_user_query(item, relic_collections)
 
-        # As long as the user input is only characters, query collections for requested item
-        userQuery = {'rewards.Intact.itemName': item}
-        axiQueryResult = axi_collection.find(userQuery)
-        neoQueryResult = neo_collection.find(userQuery)
-        mesoQueryResult = meso_collection.find(userQuery)
-        lithQueryResult = lith_collection.find(userQuery)
-
-        combinedQueries = [axiQueryResult, neoQueryResult, mesoQueryResult, lithQueryResult]
-        relicList = []
-
-        # Pull out item name, relic tier and relic name from queries, store in list
-        for queryResult in combinedQueries:
-            for indvResult in queryResult:
-                itemIndex = utility.search_list(indvResult['rewards']['Intact'], item)
-                tempItemName = indvResult['rewards']['Intact'][itemIndex]['itemName']
-                tempTier = indvResult['tier']
-                tempName = indvResult['name']
-
-                relicList.append({'tier': tempTier, 'name': tempName, 'itemName': tempItemName})
-
-        botRelicResponse = '**-----Relics Needed For {}-----**\n'.format(relicList[0]['itemName'])
-
-        # Append relic tiers/names to string that will be displayed to users
-        for item in relicList:
-            relicText = "{} {}\n".format(item['tier'], item['name'])
-            botRelicResponse += relicText
-
-        await interaction.response.send_message(botRelicResponse)
+        await interaction.response.send_message(query_results)
 
     else:
         await interaction.response.send_message("{}, please enter a sequence of letters only.".format(interaction.user.mention))
         return
+
+    return 0
 
 
 @client.event
--- utility.py
+++ utility.py
@@ -1,3 +1,21 @@
+# Only use after database connection has been made. Prevents repeat code
+import pymongo
+from pymongo import MongoClient
+
+
+def populate_collections_list(db):
+    axi_collection = db.get_collection("Axi")
+    neo_collection = db.get_collection("Neo")
+    meso_collection = db.get_collection("Meso")
+    lith_collection = db.get_collection("Lith")
+
+    return [axi_collection, neo_collection, meso_collection, lith_collection]
+
+
+'''
+Old version that I know works 
+
+# Search the lists of possible items for the item name and return the specific index with which the item belongs to
 def search_list(queryObj, itemName):
     count = 0
     for item in queryObj:
@@ -5,7 +23,21 @@ def search_list(queryObj, itemName):
             print("Item not found!")
             return -1
 
-        if item['itemName'] == itemName:
+        if item['itemName'] in itemName:
+            return count
+        else:
+            count += 1
+'''
+
+
+def search_list(queryObj, itemName):
+    count = 0
+    for item in queryObj:
+        if count > 5:
+            print("Item not found!")
+            return -1
+
+        if itemName in item['itemName']:
             return count
         else:
             count += 1
@@ -13,34 +45,89 @@ def search_list(queryObj, itemName):
 
 # Given a user query, return a formatted string that displays all relevant items and their respective relics needed
 def get_user_query(query: str, collections: list):
-    if all(char.isalpha() or char.isspace() for char in query):
+    # Add lower limit to query length to prevent user errors or very long text; prevent user from querying all items at once
+    if len(query) < 4:
+        return 'Please enter a query that contains more than 4 characters.\n'
 
-        # Add lower limit to query length to prevent user errors or very long text; prevent user from querying all items at once
-        if len(query) < 4 or query == 'prime':
-            return None
+    elif (query == 'prime') or (query == 'Prime'):
+        return 'Please do not try to query the whole database! Thank you!\n'
 
-        # As long as the user input is only characters, query collections for requested item
-        userQuery = {'rewards.Intact.itemName': {'$regex': query}}
-        combinedQueries = []
+    # As long as the user input is only characters, query collections for requested item
+    userQuery = {'rewards.Intact.itemName': {'$regex': query}}
+    combinedQueries = []
 
-        for item in collections:
-            combinedQueries.append(item.find(userQuery))
+    # Query all collections for the item, store in list for later access
+    for item in collections:
+        combinedQueries.append(item.find(userQuery))
 
-        relicList = []
+    # Eventually store all relics in dict of lists; key = item name, values = relics
+    item_dict = {}
 
-        # Pull out item name, relic tier and relic name from queries, store in list
-        for queryResult in combinedQueries:
-            for indvResult in queryResult:
-                itemIndex = search_list(indvResult['rewards']['Intact'], query)
-                tempItemName = indvResult['rewards']['Intact'][itemIndex]['itemName']
-                tempTier = indvResult['tier']
-                tempName = indvResult['name']
+    # Loop through query results from all collections and extract item name, relic name and relic tier
+    for queryResult in combinedQueries:
+        for indvResult in queryResult:
+            itemIndex = search_list(indvResult['rewards']['Intact'], query)
+            tempItemName = indvResult['rewards']['Intact'][itemIndex]['itemName']
+            tempTier = indvResult['tier']
+            tempName = indvResult['name']
 
-                relicList.append({'tier': tempTier, 'name': tempName, 'itemName': tempItemName})
+            # If key exists, append to existing item
+            if tempItemName in item_dict.keys():
+                # item_dict[tempItemName].append([tempTier, tempName])
+                item_dict[tempItemName].append({'tier': tempTier, 'name': tempName})
 
-        botRelicResponse = '**-----Relics Needed For {}-----**\n'.format(relicList[0]['itemName'])
+            # Create new key, value pair
+            else:
+                # item_dict[tempItemName] = [tempTier, tempName]
+                item_dict[tempItemName] = []
+                item_dict[tempItemName].append({'tier': tempTier, 'name': tempName})
 
-        # Append relic tiers/names to string that will be displayed to users
-        for item in relicList:
-            relicText = "{} {}\n".format(item['tier'], item['name'])
-            botRelicResponse += relicText
+    # Finally, loop through and find specific items/relics and store them in a string
+    search_results = '__**----------Item Request Results----------**__\n'
+
+    for primed_item in item_dict:
+        search_results += '**-----{}-----**\n'.format(primed_item)
+        curr_item_relics = item_dict[primed_item]
+
+        for relic in curr_item_relics:
+            temp_string = '{} {}\n'.format(relic['tier'], relic['name'])
+            search_results += temp_string
+
+        search_results += '\n'
+
+    print(search_results)
+
+    '''
+    relicList = []
+
+    # Pull out item name, relic tier and relic name from queries, store in list
+    for queryResult in combinedQueries:
+        for indvResult in queryResult:
+            itemIndex = search_list(indvResult['rewards']['Intact'], query)
+            tempItemName = indvResult['rewards']['Intact'][itemIndex]['itemName']
+            tempTier = indvResult['tier']
+            tempName = indvResult['name']
+
+            relicList.append({'tier': tempTier, 'name': tempName, 'itemName': tempItemName})
+
+    botRelicResponse = '**-----Relics Needed For {}-----**\n'.format(relicList[0]['itemName'])
+
+    # Append relic tiers/names to string that will be displayed to users
+    for item in relicList:
+        relicText = "{} {}\n".format(item['tier'], item['name'])
+        botRelicResponse += relicText
+    '''
+    return search_results
+
+
+def main():
+    db_client = MongoClient("mongodb://localhost:27017/")
+    relic_db = db_client['WarframeRelics']
+    relic_collections = populate_collections_list(relic_db)
+
+    test_results = get_user_query('prime', relic_collections)
+
+    print(test_results)
+
+
+main()
